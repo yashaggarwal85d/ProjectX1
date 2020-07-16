dfe82cafeaf0924bd40774270a5c3bd35f9862ae
yash <yashaggarwal85d@gmail.com>
2020-07-16 15:13:47 +0530

    basic login and logout has beet set up

---
+++ student_social/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import views as auth_views
+from . import views
+from django.urls import path,include
+from django.contrib import admin
+
+app_name='accounts'
+urlpatterns = [
+    path('login/',auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name="logout"),
+    path('signup/',views.SignUp.as_view(),name='signup'),
+]
---
+++ student_social/student_social/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+
+class HomePage(TemplateView):
+    template_name='index.html'
+
+class TestPage(TemplateView):
+    template_name='test.html'
+
+class ThanksPage(TemplateView):
+    template_name='thanks.html'
