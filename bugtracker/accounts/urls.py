7fb5bb04b7310b4c6770fc07be366f104914b962
yash <yashaggarwal85d@gmail.com>
2020-07-22 22:09:59 +0530

    .

--- bugtracker/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('register/',views.register,name='register'),
-    path('login/',views.loginUser,name='login'),
-    path('logout/',views.logoutUser,name='logout'),
-]
