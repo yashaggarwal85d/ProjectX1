b4be571c9921aac8b00a434347ec0a1ce563ab36
yash <yashaggarwal85d@gmail.com>
2020-07-22 16:02:11 +0530

    .

--- bugtracker/bugtracker/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(requests):
-    return render(requests,template_name='dashboard.html')
--- bugtracker/issues/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IssuesConfig(AppConfig):
-    name = 'issues'
