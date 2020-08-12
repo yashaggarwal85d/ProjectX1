a47511a3869a1e52fb0c2278a8fc6cefee8044fd
yash <yashaggarwal85d@gmail.com>
2020-08-12 02:49:30 -0700

    project stable built

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/static/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
