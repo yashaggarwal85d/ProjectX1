30f01a4728d9feaff2fe46beeeb0e43f76a85391
yash <yashaggarwal85d@gmail.com>
2020-09-29 16:18:12 +0530

    Firebase error

--- static/js/firebaseconfig.js
+++ static/js/firebaseconfig.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDc-Sr2mQm-owpyohgvywIOHSpTvFkjVUI",
-    authDomain: "projectx1-1.firebaseapp.com",
-    databaseURL: "https://projectx1-1.firebaseio.com",
-    projectId: "projectx1-1",
-    storageBucket: "projectx1-1.appspot.com",
-    messagingSenderId: "931703694027",
-    appId: "1:931703694027:web:5a72c370bdc2f69a37714e",
-    measurementId: "G-42FNCN4007"
+  apiKey: "AIzaSyDc-Sr2mQm-owpyohgvywIOHSpTvFkjVUI",
+  authDomain: "projectx1-1.firebaseapp.com",
+  databaseURL: "https://projectx1-1.firebaseio.com",
+  projectId: "projectx1-1",
+  storageBucket: "projectx1-1.appspot.com",
+  messagingSenderId: "931703694027",
+  appId: "1:931703694027:web:a461421a59d075c537714e",
+  measurementId: "G-QQNQLP5G1T"
 };
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
