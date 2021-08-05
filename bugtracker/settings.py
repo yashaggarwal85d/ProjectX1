53fb06f58b3211da83f425b4aa4ff4c1869654de
Yash Aggarwal <yashaggarwal85d@gmail.com>
2021-08-05 13:37:57 +0530

    Debug true

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#mxh!mkoa!!%fup6pq(*l)t*p4!go2c7m82j=b^v=cla)0=e^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['projectx-1.herokuapp.com','127.0.0.1','0.0.0.0']
 
